6666aa99a4c0d397bf6bbe62765260c9a046b424
Juho Kim <juho-kim@outlook.com>
2024-04-26 03:42:52 -0400

    Add more logos

--- js/script.js
+++ js/script.js
@@ -1,52 +1,26 @@
 'use strict'
 
-const minColorComponent = 225
-const maxColorComponent = 255
-const lineCount = 3
-const pointCount = 100
-const lineMultiplier = 100
-const pointIndexMultiplier = 0.025
-const millisMultiplier = 0.0001
-const minHeightMultiplier = 0.2
-const maxHeightMultiplier = 0.8
-
-function setup() {
-	const canvas = createCanvas(windowWidth, windowHeight)
-
-	canvas.parent('content')
-	frameRate(Infinity)
-}
-
-function draw() {
-	resizeCanvas(windowWidth, windowHeight)
-	background(maxColorComponent)
-	noStroke()
-	
-	let heights = []
-
-	for (let i = 0; i < pointCount; ++i)
-		heights.push(windowHeight)
-
-	for (let i = 0; i < lineCount; ++i) {
-		for (let j = 0; j < pointCount; ++j) {
-			let x = i * lineMultiplier
-			let y = j * pointIndexMultiplier
-			let z = millis() * millisMultiplier
-			heights[j] = heights[j] * (minHeightMultiplier + (maxHeightMultiplier - minHeightMultiplier) * noise(x, y, z))
-		}
-
-		fill(map(i + 1, 0, lineCount, maxColorComponent, minColorComponent))
-
-		for (let j = 0; j < pointCount - 1; ++j) {
-			let xBegin = map(j, 0, pointCount - 1, 0, windowWidth)
-			let xEnd = map(j + 1, 0, pointCount - 1, 0, windowWidth)
-
-			beginShape()
-			vertex(xBegin, windowHeight)
-			vertex(xBegin, windowHeight - heights[j])
-			vertex(xEnd, windowHeight - heights[j + 1])
-			vertex(xEnd, windowHeight)
-			endShape()
-		}
-	}
-}
+const logos = [
+	{
+		src: '/images/john-titor-insignia.png',
+		alt: 'Titor\'s purported military insignia',
+		href: 'https://en.wikipedia.org/wiki/John_Titor',
+	},
+	{
+		src: '/images/roman-dodecahedron.png',
+		alt: 'Roman bronze dodecahedron found in Tongeren, Gallo-Roman Museum, Tongeren',
+		href: 'https://en.wikipedia.org/wiki/Roman_dodecahedron',
+	},
+	{
+		src: '/images/copland-os.png',
+		alt: 'Logo of the Copland OS, depicted in Serial Experiments Lain',
+		href: 'https://en.wikipedia.org/wiki/Copland_(operating_system)',
+	},
+]
+const index = Math.floor(Math.random() * logos.length)
+const logo = logos[index]
+const logoImage = document.getElementById('logo-image')
+const logoAnchor = document.getElementById('logo-anchor')
+logoImage.src = logo.src
+logoImage.alt = logo.alt
+logoAnchor.href = logo.href
---
+++ js/sketch.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const minColorComponent = 225
+const maxColorComponent = 255
+const lineCount = 3
+const pointCount = 100
+const lineMultiplier = 100
+const pointIndexMultiplier = 0.025
+const millisMultiplier = 0.0001
+const minHeightMultiplier = 0.2
+const maxHeightMultiplier = 0.8
+
+function setup() {
+	const canvas = createCanvas(windowWidth, windowHeight)
+
+	canvas.parent('content')
+	frameRate(Infinity)
+}
+
+function draw() {
+	resizeCanvas(windowWidth, windowHeight)
+	background(maxColorComponent)
+	noStroke()
+	
+	let heights = []
+
+	for (let i = 0; i < pointCount; ++i)
+		heights.push(windowHeight)
+
+	for (let i = 0; i < lineCount; ++i) {
+		for (let j = 0; j < pointCount; ++j) {
+			let x = i * lineMultiplier
+			let y = j * pointIndexMultiplier
+			let z = millis() * millisMultiplier
+			heights[j] = heights[j] * (minHeightMultiplier + (maxHeightMultiplier - minHeightMultiplier) * noise(x, y, z))
+		}
+
+		fill(map(i + 1, 0, lineCount, maxColorComponent, minColorComponent))
+
+		for (let j = 0; j < pointCount - 1; ++j) {
+			let xBegin = map(j, 0, pointCount - 1, 0, windowWidth)
+			let xEnd = map(j + 1, 0, pointCount - 1, 0, windowWidth)
+
+			beginShape()
+			vertex(xBegin, windowHeight)
+			vertex(xBegin, windowHeight - heights[j])
+			vertex(xEnd, windowHeight - heights[j + 1])
+			vertex(xEnd, windowHeight)
+			endShape()
+		}
+	}
+}
